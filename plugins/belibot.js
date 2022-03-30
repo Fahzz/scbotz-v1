68d3e699d5614c5e7fb1d410d57ec69c561012a0
Fahzz <101871868+Fahzz@users.noreply.github.com>
2022-03-31 07:10:09 +0800

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -3,8 +3,7 @@ let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
 ╠➥ *1 DAY* :      *FREE.*
 ╠➥ *1 Minggu* :      *Rp 5.000*
-╠➥ *2 Minggu* : *Rp 10.000*
-╠➥ *PERMANEN* :   *Rp 15.000*
+╠➥ *PERMANEN* :   *Rp 10.000*
 ║
 ╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
 ╠➥ Dana/Gopay/Pulsa
